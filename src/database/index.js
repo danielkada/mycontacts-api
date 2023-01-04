894042073525b6385c01bd2b7199630fb1009ce1
Kadã <dgames201615@outlook.com>
2023-01-04 05:42:12 -0300

    bonus: unlocking multiple origins in CORS

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'kadadniel',
-  password: 'refemdomedo',
-  database: 'mycontacts',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 client.connect();
